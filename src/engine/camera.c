aac852a751b70ecf1a979b35fa1f8f3b70241362
Developer <brandonleequintin@gmail.com>
2026-06-11 15:29:03 +0900

    Add zoom support to camera with lerp and clamping

--- src/engine/camera.c
+++ src/engine/camera.c
@@ -9,14 +9,20 @@ void camera_init(Camera *cam, int world_w, int world_h) {
     cam->world_w = world_w;
     cam->world_h = world_h;
     cam->lerp_speed = 5.0f;
+    cam->zoom = 1.0f;
+    cam->zoom_target = 1.0f;
 }
 
 void camera_update(Camera *cam, float dt) {
     cam->x += (cam->target_x - cam->x) * cam->lerp_speed * dt;
     cam->y += (cam->target_y - cam->y) * cam->lerp_speed * dt;
+    cam->zoom += (cam->zoom_target - cam->zoom) * cam->lerp_speed * dt;
 
-    float half_w = g_screen_w / 2.0f;
-    float half_h = g_screen_h / 2.0f;
+    if (cam->zoom < 0.5f) cam->zoom = 0.5f;
+    if (cam->zoom > 2.0f) cam->zoom = 2.0f;
+
+    float half_w = g_screen_w / (2.0f * cam->zoom);
+    float half_h = g_screen_h / (2.0f * cam->zoom);
 
     if (cam->x - half_w < 0.0f)
         cam->x = half_w;
@@ -34,11 +40,17 @@ void camera_set_target(Camera *cam, float x, float y) {
 }
 
 void camera_world_to_screen(Camera *cam, float wx, float wy, int *sx, int *sy) {
-    *sx = (int)(wx - cam->x + g_screen_w / 2.0f);
-    *sy = (int)(wy - cam->y + g_screen_h / 2.0f);
+    *sx = (int)(wx - cam->x + g_screen_w / (2.0f * cam->zoom));
+    *sy = (int)(wy - cam->y + g_screen_h / (2.0f * cam->zoom));
 }
 
 void camera_screen_to_world(Camera *cam, int sx, int sy, int *wx, int *wy) {
-    *wx = (int)((float)sx + cam->x - g_screen_w / 2.0f);
-    *wy = (int)((float)sy + cam->y - g_screen_h / 2.0f);
+    *wx = (int)((float)sx * cam->zoom + cam->x - g_screen_w / 2.0f);
+    *wy = (int)((float)sy * cam->zoom + cam->y - g_screen_h / 2.0f);
+}
+
+void camera_set_zoom(Camera *cam, float zoom) {
+    if (zoom < 0.5f) zoom = 0.5f;
+    if (zoom > 2.0f) zoom = 2.0f;
+    cam->zoom_target = zoom;
 }
--- src/engine/camera.h
+++ src/engine/camera.h
@@ -6,6 +6,8 @@ typedef struct {
     float target_x, target_y;
     int world_w, world_h;
     float lerp_speed;
+    float zoom;
+    float zoom_target;
 } Camera;
 
 void camera_init(Camera *cam, int world_w, int world_h);
@@ -13,5 +15,6 @@ void camera_update(Camera *cam, float dt);
 void camera_set_target(Camera *cam, float x, float y);
 void camera_world_to_screen(Camera *cam, float wx, float wy, int *sx, int *sy);
 void camera_screen_to_world(Camera *cam, int sx, int sy, int *wx, int *wy);
+void camera_set_zoom(Camera *cam, float zoom);
 
 #endif
